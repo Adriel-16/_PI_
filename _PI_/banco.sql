97090de53c72f228bdb8e0bfa0f6186e937b013f
Adriel Loan da Cruz <adriel.docabelinho@gmail.com>
2025-01-28 17:26:27 -0300

    Add files via upload

---
+++ _PI_/banco.sql
@@ -0,0 +1,37 @@
+CREATE DATABASE Projeto;
+
+CREATE TABLE Usuario (
+   	
+);
+
+CREATE TABLE Contatos (
+    
+);
+
+
+SELECT * FROM Agenda WHERE nome like 'A%';
+SELECT * FROM Agenda WHERE nome like 'B%';
+SELECT * FROM Agenda WHERE nome like 'C%';
+SELECT * FROM Agenda WHERE nome like 'D%';
+SELECT * FROM Agenda WHERE nome like 'E%';
+SELECT * FROM Agenda WHERE nome like 'F%';
+SELECT * FROM Agenda WHERE nome like 'G%';
+SELECT * FROM Agenda WHERE nome like 'H%';
+SELECT * FROM Agenda WHERE nome like 'I%';
+SELECT * FROM Agenda WHERE nome like 'J%';
+SELECT * FROM Agenda WHERE nome like 'K%';
+SELECT * FROM Agenda WHERE nome like 'L%';
+SELECT * FROM Agenda WHERE nome like 'M%';
+SELECT * FROM Agenda WHERE nome like 'N%';
+SELECT * FROM Agenda WHERE nome like 'O%';
+SELECT * FROM Agenda WHERE nome like 'P%';
+SELECT * FROM Agenda WHERE nome like 'Q%';
+SELECT * FROM Agenda WHERE nome like 'R%';
+SELECT * FROM Agenda WHERE nome like 'S%';
+SELECT * FROM Agenda WHERE nome like 'T%';
+SELECT * FROM Agenda WHERE nome like 'U%';
+SELECT * FROM Agenda WHERE nome like 'V%';
+SELECT * FROM Agenda WHERE nome like 'W%';
+SELECT * FROM Agenda WHERE nome like 'X%';
+SELECT * FROM Agenda WHERE nome like 'Y%';
+SELECT * FROM Agenda WHERE nome like 'Z%';
